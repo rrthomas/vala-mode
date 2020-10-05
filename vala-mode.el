3921540a3fd4a4f7d2870c5273b25527b5a48f11
Reuben Thomas <rrt@sc3d.org>
2020-10-05 09:36:25 +0100

    Remove old documentation, including broken link
    No need to specify that we require CC Mode 5.30, as that was released in
    2003; it’s likely that by now we require a newer version, and no user’s
    likely to be stuck with an older one.

--- vala-mode.el
+++ vala-mode.el
@@ -28,13 +28,7 @@
 ;;
 ;;    See http://live.gnome.org/Vala for details about Vala language.
 ;;
-;;    This is a separate mode to implement the Vala constructs and
-;;    font-locking. It is mostly the csharp-mode from
-;;    http://mfgames.com/linux/csharp-mode with vala specific keywords
-;;    and filename suffixes.
-;;
-;;    Note: The interface used in this file requires CC Mode 5.30 or
-;;    later.
+;;    This mode was based on Dylan Moonfire's csharp-mode.
 
 ;; This is a copy of the function in cc-mode which is used to handle
 ;; the eval-when-compile which is needed during other times.
